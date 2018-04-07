0cca9f21a94988a7f228879b53e6785b04dabf12
Stanislav Kharitonov <q3introx@mail.ru>
2018-04-07 17:02:26 +0400

    Step 2 refactor

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,10 @@
 import fs from 'fs';
+import path from 'path';
 import gendiff from '../src';
 
-const beforeFileJsonPath = './__tests__/__fixtures__/gendiff-json/before.json';
-const afterFileJsonPath = './__tests__/__fixtures__/gendiff-json/after.json';
-const correctFilePath = './__tests__/__fixtures__/gendiff-json/correct.txt';
+const beforeFileJsonPath = path.resolve(__dirname, '__fixtures__/gendiff-json/before.json');
+const afterFileJsonPath = path.resolve(__dirname, '__fixtures__/gendiff-json/after.json');
+const correctFilePath = path.resolve(__dirname, '__fixtures__/gendiff-json/correct.txt');
 
 const correctDiff = fs.readFileSync(correctFilePath).toString();
 
@@ -11,38 +12,38 @@ test('gendiff-json', () => {
   expect(gendiff(beforeFileJsonPath, afterFileJsonPath)).toBe(correctDiff);
 });
 
-const beforeFileYamlPath = './__tests__/__fixtures__/gendiff-yaml/before.yml';
-const afterFileYamlPath = './__tests__/__fixtures__/gendiff-yaml/after.yaml';
+const beforeFileYamlPath = path.resolve(__dirname, '__fixtures__/gendiff-yaml/before.yml');
+const afterFileYamlPath = path.resolve(__dirname, '__fixtures__/gendiff-yaml/after.yaml');
 
 test('gendiff-yaml', () => {
   expect(gendiff(beforeFileYamlPath, afterFileYamlPath)).toBe(correctDiff);
 });
 
-const beforeFileIniPath = './__tests__/__fixtures__/gendiff-ini/before.ini';
-const afterFileIniPath = './__tests__/__fixtures__/gendiff-ini/after.ini';
+const beforeFileIniPath = path.resolve(__dirname, '__fixtures__/gendiff-ini/before.ini');
+const afterFileIniPath = path.resolve(__dirname, '__fixtures__/gendiff-ini/after.ini');
 
 test('gendiff-ini', () => {
   expect(gendiff(beforeFileIniPath, afterFileIniPath)).toBe(correctDiff);
 });
 
-const beforeAstFileJsonPath = './__tests__/__fixtures__/gendiff-json-ast/before.json';
-const afteAstrFileJsonPath = './__tests__/__fixtures__/gendiff-json-ast/after.json';
-const correctAstFilePath = './__tests__/__fixtures__/gendiff-json-ast/correct.txt';
+const beforeAstFileJsonPath = path.resolve(__dirname, '__fixtures__/gendiff-json-ast/before.json');
+const afteAstrFileJsonPath = path.resolve(__dirname, '__fixtures__/gendiff-json-ast/after.json');
+const correctAstFilePath = path.resolve(__dirname, '__fixtures__/gendiff-json-ast/correct.txt');
 const correctDiffAst = fs.readFileSync(correctAstFilePath).toString();
 
 test('gendiff-json-ast', () => {
   expect(gendiff(beforeAstFileJsonPath, afteAstrFileJsonPath)).toBe(correctDiffAst);
 });
 
-const beforeAstFileYamlPath = './__tests__/__fixtures__/gendiff-yaml-ast/before.yaml';
-const afteAstrFileYamlPath = './__tests__/__fixtures__/gendiff-yaml-ast/after.yml';
+const beforeAstFileYamlPath = path.resolve(__dirname, '__fixtures__/gendiff-yaml-ast/before.yaml');
+const afteAstrFileYamlPath = path.resolve(__dirname, '__fixtures__/gendiff-yaml-ast/after.yml');
 
 test('gendiff-yaml-ast', () => {
   expect(gendiff(beforeAstFileYamlPath, afteAstrFileYamlPath)).toBe(correctDiffAst);
 });
 
-const beforeAstFileIniPath = './__tests__/__fixtures__/gendiff-ini-ast/before.ini';
-const afteAstrFileIniPath = './__tests__/__fixtures__/gendiff-ini-ast/after.ini';
+const beforeAstFileIniPath = path.resolve(__dirname, '__fixtures__/gendiff-ini-ast/before.ini');
+const afteAstrFileIniPath = path.resolve(__dirname, '__fixtures__/gendiff-ini-ast/after.ini');
 
 test('gendiff-ini-ast', () => {
   expect(gendiff(beforeAstFileIniPath, afteAstrFileIniPath)).toBe(correctDiffAst);
